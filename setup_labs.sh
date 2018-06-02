8560a60b6f37c20a82e580032b371f09f94677b0
Brent Laster <Brent.Laster@sas.com>
2018-06-02 05:52:33 -0400

    update for latest approach
    Change-Id: Ie7ee5c085a4f0b188a851bb9a1ab31a158bb1f20

--- setup_labs.sh
+++ setup_labs.sh
@@ -3,11 +3,8 @@
 # change permissions on docker.sock to simplify setup
 chmod 777 /run/docker.sock
 
-# pull down the necessary image
-docker pull bclaster/dc_jenkins:1.5
-
-# start the image running
-docker service create -e JAVA_OPTS="-Dhudson.Main.development=true -Djenkins.install.runSetupWizard=false" -p 8080 --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock  --name jenkins_lab bclaster/dc_jenkins:1.5
+# start the services
+docker stack deploy -c docker-compose.yml ci
 
 # grab the gradle version
 wget https://services.gradle.org/distributions/gradle-3.4.1-bin.zip
@@ -15,5 +12,4 @@ wget https://services.gradle.org/distributions/gradle-3.4.1-bin.zip
 # put the gradle version in place
 sudo unzip -d /usr/share gradle-3.4.1-bin.zip
 
-# copy the key out for ease of access
-cp /root/pwd_jenkins/id_rsa.pub /root/id_rsa.pub
+
